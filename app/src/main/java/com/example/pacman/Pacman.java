c00bbd4cb2e0e1ac2b80b60101b4a007c62eb739
carlitox477 <carman_boca@hotmail.com>
2020-03-22 04:05:34 -0300

    Back to normality

---
+++ app/src/main/java/com/example/pacman/Pacman.java
@@ -0,0 +1,11 @@
+package com.example.pacman;
+
+public class Pacman extends Character {
+    private boolean superPacman; //if true pacman can eat ghost
+
+
+    public Pacman() {
+        super("pacman");
+    }
+
+}
